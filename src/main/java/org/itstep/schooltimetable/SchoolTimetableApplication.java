283a9c9dbea90e3bfbbf8270c8de17d383c24a67
Kiril <jolymod96@gmail.com>
2023-07-02 12:56:36 +0300

    Initial commit

---
+++ src/main/java/org/itstep/schooltimetable/SchoolTimetableApplication.java
@@ -0,0 +1,13 @@
+package org.itstep.schooltimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTimetableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/itstep/schooltimetable/SchoolTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itstep.schooltimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolTimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
